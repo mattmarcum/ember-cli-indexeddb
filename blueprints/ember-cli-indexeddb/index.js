84ba90ad1084380b817d4fc5d191dd38982ff38d
Matthew Marcum <mmarcum@softlayer.com>
2014-12-31 16:31:18 -0600

    updates to addon bp

--- blueprints/ember-cli-indexeddb/index.js
+++ blueprints/ember-cli-indexeddb/index.js
@@ -1,11 +1,18 @@
-var Blueprint = require( 'ember-cli/lib/models/blueprint' );
-var Promise    = require('ember-cli/lib/ext/promise');
+var Blueprint   = require( 'ember-cli/lib/models/blueprint' );
+var Promise     = require('ember-cli/lib/ext/promise');
+var fs          = require('fs-extra');
+var path        = require('path');
+var EOL         = require('os').EOL;
+var SilentError = require('ember-cli/lib/errors/silent');
+
 
 module.exports = {
     description: 'Set up Ember-Cli-IndexedDB.',
 
     availableOptions: [
-        { name: 'adapter', default: 'application' }
+        { name: 'databaseName' },
+        { name: 'adapter', default: 'application' },
+        { name: 'migration', default: 'initial-migration' }
     ],
 
     normalizeEntityName: function() {
@@ -15,11 +22,11 @@ module.exports = {
     },
 
     install: function( options ){
-        //TODO parse options
+        this.project = options.project;
 
         return Promise.resolve().then(function(){
             var blueprint = Blueprint.lookup( 'idb-initializer', {
-                paths: this.project.blueprintLookupPaths(),
+                paths: options.project.blueprintLookupPaths(),
             });
 
             return Promise.resolve().then(function(){
@@ -27,31 +34,32 @@ module.exports = {
             });
         }).then(function(){
             var blueprint = Blueprint.lookup( 'idb-migration', {
-                paths: this.project.blueprintLookupPaths(),
+                paths: options.project.blueprintLookupPaths(),
             });
+
+            options.entity.name = options.migration || 'initial-migration';
 
             return Promise.resolve().then(function(){
                 return blueprint.install( options );
             });
         }).then(function(){
             var blueprint = Blueprint.lookup( 'idb-adapter', {
-                paths: this.project.blueprintLookupPaths(),
+                paths: options.project.blueprintLookupPaths(),
             });
+
+            options.entity.name = options.migration || 'application';
 
             return Promise.resolve().then(function(){
                 return blueprint.install( options );
             });
-        });
+        }).then(function(){
+            if( options.databaseName ){
+                return this.insertIntoFile(
+                    'config/environment.js',
+                    "ENV.APP.indexedDBName = '"+ options.databaseName +"';"+EOL,
+                    { before: 'return ENV;' }
+                );
+            }
+        }.bind(this));
     }
-
-    // locals: function(options) {
-    //   // Return custom template variables here.
-    //   return {
-    //     foo: options.entity.options.foo
-    //   };
-    // }
-
-    // afterInstall: function(options) {
-    //   // Perform extra work here.
-    // }
 };
--- blueprints/idb-migration/index.js
+++ blueprints/idb-migration/index.js
@@ -1,8 +1,5 @@
 module.exports = {
     description: 'Create a migration for IndexedDB.',
-    normalizeEntityName: function( name ) {
-        return 'blah';
-    },
     /*
     1. figure out what the current migration version is
     2.

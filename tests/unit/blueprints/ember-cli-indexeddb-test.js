3dda68f780dc1084ac93072e42b8122df0fecdde
Matt Marcum <matthew.marcum@activenetwork.com>
2015-05-04 17:17:24 -0500

    finished blue print test for ember-cli-indexeddb blueprint

--- tests/unit/blueprints/ember-cli-indexeddb-test.js
+++ tests/unit/blueprints/ember-cli-indexeddb-test.js
@@ -13,7 +13,13 @@ var EOL               = require('os').EOL;
 
 var blueprintsFolder  = path.resolve(root, 'blueprints');
 var ecidbBlueprint    = path.join(blueprintsFolder, 'ember-cli-indexeddb');
-var blueprintClass    = require('ember-cli/lib/models/blueprint')
+var blueprintClass    = require('ember-cli/lib/models/blueprint');
+
+var basicBlueprintFiles = [
+  "app/",
+  "app/migrations/",
+  "app/migrations/migration-manager.js"
+];
 
 describe('Ember-cli-indexeddb blueprint', function() {
   var blueprint;
@@ -45,20 +51,18 @@ describe('Ember-cli-indexeddb blueprint', function() {
       .then(function() {
         var actualFiles = walkSync(tmpdir).sort();
         var output = ui.output.trim().split(EOL);
-
         expect(output.shift()).to.match(/^installing/);
-       // expect(output.shift()).to.match(/create.* app/migrations/migration-manager.js/);
+        expect(output.shift()).to.match(/create.*/);// );
         expect(output.length).to.equal(0);
 
         expect(actualFiles).to.deep.equal(basicBlueprintFiles);
 
         expect( function(){
-          fs.readFile(path.join(tmpdir , 'test.txt'), 'utf-8',
+          fs.readFile(path.join(tmpdir , 'app/migrations/migration-manager.js'), 'utf-8',
               function(err, content){
                   if(err){
                       throw 'error';
                   }
-                  expect(content).to.match(/I AM TESTY/);
               });
           }
         ).not.to.throw();
